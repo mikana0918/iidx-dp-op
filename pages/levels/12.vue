4ac087719144767f8e97f8b56949e954b401ec73
mikana0918 <shogo.kusuhara@gmail.com>
2021-04-25 16:52:31 +0900

    feat: add support for Mobile Compatibility

--- pages/levels/12.vue
+++ pages/levels/12.vue
@@ -1,21 +1,27 @@
 <template>
-  <div>
-    <v-card-title>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="dpOptionsData"
-      :items-per-page="20"
-      class="elevation-1"
-      :search="search"
-    ></v-data-table>
+  <div class="container">
+    <div style="position: fixed; width: 88%">
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+    </div>
+    <div style="padding-top: 30%">
+      <v-data-table
+        :headers="headers"
+        :items="dpOptionsData"
+        items-per-page="20"
+        class="elevation-1"
+        :search="search"
+        mobile-breakpoint="0"
+        fixed-header="true"
+      ></v-data-table>
+    </div>
   </div>
 </template>
 
@@ -33,10 +39,6 @@ export default Vue.extend({
           value: 'name',
         },
         { text: 'オプション(Options)', value: 'suggest' },
-        // { text: 'Fat (g)', value: 'fat' },
-        // { text: 'Carbs (g)', value: 'carbs' },
-        // { text: 'Protein (g)', value: 'protein' },
-        // { text: 'Iron (%)', value: 'iron' },
       ],
     }
   },
