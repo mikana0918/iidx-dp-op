a41d730f842a0dc54e2066245d7eec030fb24eb8
mikana0918 <shogo.kusuhara@gmail.com>
2021-05-18 00:03:43 +0900

    feat: :sparkles: :pensive: I don't think this store name is not good but just add DBR store for feature

--- store/dbr.ts
+++ store/dbr.ts
@@ -1,4 +1,5 @@
 import { getterTree, mutationTree, actionTree } from 'typed-vuex'
+import { firestore } from '~/plugins/firebase/firestore'
 
 export const state = () => ({
   dbrListForKaiden: undefined as
@@ -7,6 +8,7 @@ export const state = () => ({
         dbrList: any
       }
     | undefined,
+  initialDataForKaidenTenkuu: undefined as { succeeded: boolean } | undefined,
 })
 
 export const getters = getterTree(state, {})
@@ -24,20 +26,69 @@ export const mutations = mutationTree(state, {
       dbrList: [],
     }
   },
+  SET_INITIAL_DBR_DATA_FOR_KAIDEN_TENKUU_SUCCESS(state) {
+    state.initialDataForKaidenTenkuu = {
+      succeeded: true,
+    }
+  },
+  SET_INITIAL_DBR_DATA_FOR_KAIDEN_TENKUU_FAIL(state) {
+    state.initialDataForKaidenTenkuu = {
+      succeeded: false,
+    }
+  },
 })
 
 export const actions = actionTree(
   { state, getters, mutations },
   {
-    getDBRListForKaiden(this, { commit }) {
-      try {
-        commit('SET_DBR_LIST_DATA_SUCCESS', {
-          data: process.env.dbrListForKaiden,
+    /**
+     * 皆伝 - 天空の夜明け DBR Practice List
+     * Get stored data.
+     */
+    getMyDBRListForKaidenForTenkuu(this, { commit }, { uid }: { uid: string }) {
+      firestore
+        .collection('dbr_list_for_kaiden_for_tenkuu')
+        .doc(uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            commit('SET_DBR_LIST_DATA_SUCCESS', { data: doc.data()?.dbr_data })
+          } else {
+            // doc.data() will be undefined in this case
+            console.log('No such document! / dbr 皆伝:天空の夜明け ')
+            // set default if user have no data.
+            this.app.$accessor.dbr.setDefaultMyDBRListForKaidenForTenkuu({
+              uid,
+            })
+          }
         })
-      } catch (e) {
-        console.warn(e)
-        commit('SET_DBR_LIST_DATA_FAIL', e.response)
-      }
+        .catch((error) => {
+          console.log('Error getting document:', error)
+          commit('SET_DBR_LIST_DATA_FAIL')
+        })
+    },
+    /**
+     * 皆伝 - 天空の夜明け DBR Practice List
+     * Store default list.
+     */
+    setDefaultMyDBRListForKaidenForTenkuu(
+      this,
+      { commit },
+      { uid }: { uid: string }
+    ) {
+      firestore
+        .collection('dbr_list_for_kaiden_for_tenkuu')
+        .doc(uid)
+        .set({
+          dbr_data: process.env.dbrListForKaiden,
+        })
+        .then(() => {
+          commit('SET_INITIAL_DBR_DATA_FOR_KAIDEN_TENKUU_SUCCESS')
+        })
+        .catch((error) => {
+          console.error('Error adding document: ', error)
+          commit('SET_INITIAL_DBR_DATA_FOR_KAIDEN_TENKUU_FAIL')
+        })
     },
   }
 )
