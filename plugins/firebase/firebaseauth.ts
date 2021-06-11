d12fbb65da972e5e72b1ef7729b1acbebbd99878
mikana0918 <shogo.kusuhara@gmail.com>
2021-06-11 16:17:53 +0900

    refactor: change firebase auth store method

--- plugins/firebase/firebaseauth.ts
+++ plugins/firebase/firebaseauth.ts
@@ -1,8 +1,25 @@
 import firebase from '~/plugins/firebase/firebase'
 import 'firebase/auth'
+import { logger } from '~/logging/index'
 
-export const auth = firebase.auth()
+const auth = firebase.auth()
 
 if (process.env.ENV === 'local') {
   auth.useEmulator('localhost:4000')
 }
+
+auth
+  .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    // return firebase.auth().signInWithEmailAndPassword(email, password)
+  })
+  .catch((error) => {
+    logger.error(`Firebase auth catched error ${error}`)
+  })
+
+export { auth }
