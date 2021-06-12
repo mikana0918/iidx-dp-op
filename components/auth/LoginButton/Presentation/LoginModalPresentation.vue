40deed3afd442f22d053572c497e8f5e30bfef66
mikana0918 <shogo.kusuhara@gmail.com>
2021-06-12 15:03:06 +0900

    feat: :lipstick: create base component for v-card / use divider

--- components/auth/LoginButton/Presentation/LoginModalPresentation.vue
+++ components/auth/LoginButton/Presentation/LoginModalPresentation.vue
@@ -1,19 +1,26 @@
 <template>
-  <v-card>
+  <Card>
     <v-card-title>Sign in</v-card-title>
-    <v-divider></v-divider>
+    <Divider></Divider>
     <v-container fill-height fluid>
       <v-row align="center" justify="center" @click="googleLogin">
         <!-- Sign in with Google  -->
         <img src="~/assets/icons/btn_google_signin_light_focus_web.png" />
       </v-row>
     </v-container>
-  </v-card>
+  </Card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Divider from '~/components/base/divider/Divider.vue'
+import Card from '~/components/base/card/Card.vue'
+
 export default Vue.extend({
+  components: {
+    Divider,
+    Card,
+  },
   methods: {
     showDialog(show: boolean) {
       this.$emit('show-dialog', show)
